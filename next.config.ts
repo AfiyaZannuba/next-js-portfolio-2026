5fb02140d8f1d274dfe40dbf550a7c60923978ae
AfiyaZannuba <afiya.zannubaa@gmail.com>
2026-07-17 00:10:17 +0400

    configuration issues are resolved. Errors that are popped once running npm run build. Errors were webpack/turbopack choosing issue and configuration file code

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   webpack(config) {
-    config.module.rules.push({
+    config.module?.rules?.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
